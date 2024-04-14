3ffd840412595405f6b0f029a4d943c67e381ad8
mobina-ghafari <135110634+mobina-ghafari@users.noreply.github.com>
2024-04-13 22:13:49 -0700

    js+modal

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ menucolor.forEach(menucolor => {
         menucolor.classList.toggle('my-menu-after-click');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.getElementById('myModal');
+    modal.style.display = 'block';
+  
+    const closeButton = document.querySelector('.close');
+    closeButton.addEventListener('click', function() {
+      modal.style.display = 'none';
+    });
+  
+    window.addEventListener('click', function(event) {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+  });
